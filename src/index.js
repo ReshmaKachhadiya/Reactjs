436856583f86f173d3588b2b14cc52de8ea7b8aa
Reshma <reshmakachhadiya@gmail.com>
2021-04-17 14:34:17 +0530

    version-1

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+                <>
+                <h1>
+                  Must Watch This Drama Serials
+                </h1>
+                <p>
+                   List of Pakistani Drama
+                </p>
+               <ol>
+                  <li>  Mera Dil Mera Dusmal</li>
+                  <li>  Ishqiya </li>
+                  <li>  Yakin ka safr </li>
+                  <li>  Ehd e Wafa</li>
+                  <li>  joh Tum chhaho </li>
+                </ol>
+                </>
+                ,
+              document.getElementById("root")
+
+);
